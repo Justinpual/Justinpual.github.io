bd6ddf63d96932da638f8c610d0eb9491189f0c2
Justinpual <158082773+Justinpual@users.noreply.github.com>
2024-02-08 17:39:53 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,10 +43,12 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
-    
+      
+      createPlatform(500, 600, 200, 20); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
+      createPlatform(300, 620, 200, 20) //platform 2
+      createPlatform()
+      createPlatform()
+      createPlatform()
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
